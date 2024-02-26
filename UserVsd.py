533e1165fa50487722cf359693164b69047c8cce
alja <amraktadel@ucsd.edu>
2024-02-26 11:15:40 -0800

    Add event info, jets and mets

--- UserVsd.py
+++ UserVsd.py
@@ -2,7 +2,7 @@ import ROOT
 
 ROOT.gSystem.Load("libVsdDict.so")
 
-Vfile = ROOT.TFile("UserVsd.root", "RECREATE")
+Vfile = ROOT.TFile("UserVsd-2.root", "RECREATE")
 Vtree = ROOT.TTree("VSD", "Custom plain VSD tree")
 
 pcv = ROOT.std.vector('VsdCandidate')()
@@ -11,9 +11,23 @@ Vtree.Branch("PinkCands", pcv)
 gjv = ROOT.std.vector('VsdJet')()
 Vtree.Branch("GreenJets", gjv)
 
+tmv = ROOT.std.vector('VsdMET')()
+Vtree.Branch("TestMETs", tmv)
+
+umv = ROOT.std.vector('VsdMuon')()
+Vtree.Branch("UMuon", umv)
+
+eiv = ROOT.std.vector('VsdEventInfo')()
+Vtree.Branch("EventInfo", eiv)
+
+
 for i in range(10):
+
     pcv.clear()
     gjv.clear()
+    tmv.clear()
+    umv.clear()
+    eiv.clear() # could be reused since it is a single object per event
 
     for j in range(10 + ROOT.gRandom.Integer(11)):
         cnd = ROOT.VsdCandidate(
@@ -34,6 +48,27 @@ for i in range(10):
             ROOT.gRandom.Uniform(0.05, 1))
         jet.name = f"Jet_{j}"
         gjv.push_back(jet)
+
+    for j in range( 1 ):
+        met = ROOT.VsdMET(
+            ROOT.gRandom.Uniform(0.1, 20),
+            ROOT.gRandom.Uniform(-2.5, 2.5),
+            ROOT.gRandom.Uniform(-ROOT.TMath.Pi(), ROOT.TMath.Pi()),
+            ROOT.gRandom.Uniform(0.1, 20) +10)
+        met.name = f"MET_{j}"
+        tmv.push_back(met)
+
+    for j in range(5):
+        muon = ROOT.VsdMuon(
+            ROOT.gRandom.Uniform(0.1, 20),
+            ROOT.gRandom.Uniform(-2.5, 2.5),
+            ROOT.gRandom.Uniform(-ROOT.TMath.Pi(), ROOT.TMath.Pi()),
+            (1 if ROOT.gRandom.Rndm() > 0.5 else -1))
+        muon.name = f"Muon_{j}"
+        umv.push_back(muon)
+    
+    ei = ROOT.VsdEventInfo(333, 7777, i+1000)
+    eiv.push_back(ei);
 
     Vtree.Fill()
 
